11470725c1f442d29398ddf51088a97bbd9632a4
OscarF2834 <pipesegoviaalvarado@gmail.com>
2025-07-26 22:33:36 -0500

    Primer subida del juego

---
+++ src/Juego_Ahorcado.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Juego_Ahorcado {
+    public static void main(String[] args) throws Exception {
+        //Vamos hacer el juego del ahorcado
+
+        //clase para que el usuario pueda escribir por medio de teclado
+        Scanner entrada = new Scanner(System.in);
+
+        //Declaracion de las variables
+        String palabraSecreta = "inteligencia";
+        int intentosMaximos = 10;
+        int intentos = 0;
+        boolean palabraAdivinada = false;
+
+        //Arreglos:
+        char [] letrasAdivinadas = new char[palabraSecreta.length()];
+
+        //Estructura de control de tipo iterativa
+        for (int i = 0; i < letrasAdivinadas.length; i++) {
+            letrasAdivinadas[i] = '_';
+        }
+
+        while (!palabraAdivinada && intentos < intentosMaximos) {
+
+            System.out.println("Palabra a adivinar: " + String.valueOf(letrasAdivinadas) + " " + palabraSecreta.length() + "letras");
+            System.out.println("Introduce una letra por favor");
+
+            char letra = Character.toLowerCase(entrada.next().charAt(0));
+
+            boolean letraCorrecta = false;
+
+            for (int i = 0; i < palabraSecreta.length(); i++) {
+                if (palabraSecreta.charAt(i) == letra) {
+                    letrasAdivinadas[i] = letra;
+                    letraCorrecta = true;
+                }
+            }
+
+            if (!letraCorrecta) {
+                intentos++;
+                System.out.println("Letra incorrecta, te quedan: " + (intentosMaximos - intentos) + "intentos.");
+            }
+
+            if (String.valueOf(letrasAdivinadas).equals(palabraSecreta)){
+                palabraAdivinada = true;
+                System.out.println("Felicidades, has adivinado la palabra secreta" + palabraSecreta);
+            }
+        }
+
+        if (!palabraAdivinada) {
+            System.out.println("Que pena, te has quedado sin intentos");
+        }
+
+        entrada.close();
+    }
+}
